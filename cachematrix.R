d1323f2fd4da6aa6e88ed01d42ca98a6dc6e2029
Andrew <kerr325@gmail.com>
2015-01-20 13:18:46 -0600

    updates for class requirements and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In the function below I'm setting the value of the vector (m), then getting (get)
+## the value of the vector, then setting the value of the mean (setmean), then finally getting
+## the value of the mean (getmean)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m <<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
+## Below I'm checking to see if the the mean of the matrix has already been calculated. 
+## If it has been calculated it retrieves the calculation, if not it will run the function as stated above.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
